deb8d276b5be3dfd09c0398b9fc556c98707939f
A Xiao <98532074+axiao-crud@users.noreply.github.com>
2022-11-27 20:03:33 +0800

    1.二分查找两种区间写法 2.移除元素-双指针写法

--- DataStructure/src/main/java/Array.java
+++ DataStructure/src/main/java/Array.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 /**
@@ -8,10 +9,86 @@ import java.util.logging.Logger;
 public class Array {
     private static  final  Logger log = Logger.getLogger("ArrayLogger");
     public static void main(String[] args) {
-        int [] a = {1,2,3};
-
+        int [] a = {1,3,5};
 
     }
+
+
+
+
+
+    //二分查找[left,right]
+    public static int binarySearch(int nums[],int target ){
+        int left = 0;
+        int right = nums.length-1;
+        int middle = (left+right)/2;
+        for (;left<=right;middle=(left+right)/2){
+            if(nums[middle]==target){
+                return middle;
+            }else if(nums[middle]>target){
+                right = middle-1;
+            }else  if(nums[middle]<target){
+                left = middle+1;
+            }
+
+
+
+        }
+
+        return -1;
+    }
+    //二分查找[left,right)
+    public static int binarySearch2(int nums[],int target ){
+        int left = 0;
+        int right = nums.length;
+        int middle =(left+right)/2;
+        for(;left<right;middle=(left+right)/2){
+            if(nums[middle]==target){
+                return middle;
+            }
+            if(nums[middle]>target){
+                right = middle;
+            }
+            if(nums[middle]<target){
+                left =middle+1;
+            }
+        }
+
+        return -1;
+    }
+    //二分查找拓展-查找插入元素位置
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right =nums.length-1;
+        for(;left<=right;){
+            int middle=(left+right)/2;
+            if(nums[middle]==target){
+                return middle;
+            }
+            if(nums[middle]<target){
+                left = middle+1;
+            }
+            if(nums[middle]>target){
+                right = middle-1;
+            }
+        }
+
+        return left;
+    }
+
+    //移除元素-双指针写法
+    public static int  removeElement(int []nums,int val){
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=val){
+                nums[left]=nums[right];
+                left++;
+            }
+        }
+
+        return left;
+    }
+
     
 
 }
--- DataStructure/src/main/resources/ArrayBase.markdown
+++ DataStructure/src/main/resources/ArrayBase.markdown
@@ -7,5 +7,20 @@ int [] a = {1,2,3};
 int [] a = new int[3];
 
 # 2.数组算法
-1.二分查找
-2.移除元素
+1.二分查找（衍生题：查找插入元素）
+(使用前提：有序不重复数组)
+
+
+容易错的点:
+两种写法 [left,right]
+边界条件 left<=right;
+left =middle +1;
+right=middle-1;
+
+和[left,right)
+left <right 
+left =middle+1;
+right =middle;
+
+
+2.移除元素-双指针写法
